31d41e10ebb1652a9c7550deef4c723712cae56a
ieglonew01f <ieglonew01f@gmail.com>
2017-07-22 13:27:46 +0000

    setup fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'mukundamg1993@gmail.com', password: 'mukundamg1993', password_confirmation: 'mukundamg1993')
+Setting.create!(setting_name: 'college_list', setting_value: 'BMS College of Engineering, ITM Universe')
