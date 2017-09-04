b033c28b5246d3cd3ffdcb19f561c9f962cf0d25
Mukunda Gogoi <mukundag@qubole.com>
2017-09-04 15:44:10 +0530

    fix for activity

--- app/helpers/activity_helper.rb
+++ app/helpers/activity_helper.rb
@@ -11,18 +11,21 @@ module ActivityHelper
       when "post.create"
         if a.trackable_id
           post = Post.find(a.trackable_id)
-          
-          activities.push({"message" => "shared a new <a href='/posts/#{post.id}'>post</a>", "time_stamp" => time_ago_in_words(a.created_at), "owner" => owner, "object" => post})
+          if post
+            activities.push({"message" => "shared a new <a href='/posts/#{post.id}'>post</a>", "time_stamp" => time_ago_in_words(a.created_at), "owner" => owner, "object" => post})
+          end
         end
       when "post_like.create"
         if a.trackable_id
           post = PostLike.find(a.trackable_id).post
           recipient = User.find(post.user_id)
 
-          if recipient.id === a.owner_id
-            activities.push({"message" => "liked a <a href='/posts/#{post.id}'>post</a>", "time_stamp" => time_ago_in_words(a.created_at), "owner" => owner, "object" => post})
-          else
-            activities.push({"message" => "liked <a href='/profile/#{recipient.username}'>#{recipient.first_name} #{recipient.last_name}</a>  <a href='/posts/#{post.id}'>post</a>", "time_stamp" => time_ago_in_words(a.created_at), "owner" => owner, "object" => post})  
+          if post
+            if recipient.id === a.owner_id
+              activities.push({"message" => "liked a <a href='/posts/#{post.id}'>post</a>", "time_stamp" => time_ago_in_words(a.created_at), "owner" => owner, "object" => post})
+            else
+              activities.push({"message" => "liked <a href='/profile/#{recipient.username}'>#{recipient.first_name} #{recipient.last_name}</a>  <a href='/posts/#{post.id}'>post</a>", "time_stamp" => time_ago_in_words(a.created_at), "owner" => owner, "object" => post})  
+            end
           end
         end
       when "comment.create"
@@ -30,7 +33,9 @@ module ActivityHelper
           post = Comment.find(a.trackable_id).post
           recipient = User.find(post.user_id)
 
-          activities.push({"message" => "commented on <a href='/profile/#{recipient.username}'>#{recipient.first_name} #{recipient.last_name}</a>  <a href='/posts/#{post.id}'>post</a>", "time_stamp" => time_ago_in_words(a.created_at), "owner" => owner, "object" => post}) 
+          if post
+            activities.push({"message" => "commented on <a href='/profile/#{recipient.username}'>#{recipient.first_name} #{recipient.last_name}</a>  <a href='/posts/#{post.id}'>post</a>", "time_stamp" => time_ago_in_words(a.created_at), "owner" => owner, "object" => post}) 
+          end
         end
       end
     end
